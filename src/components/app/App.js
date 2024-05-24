a53b155c1dd2be85df5057f800932c0b93592b84
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-05-24 11:35:28 +0300

    rewritten from class components to functional components

---
+++ src/components/app/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundery from "../errorBoundery/ErrorBoundery";
+
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+
+  const [selectedChar, setChar] = useState(null);
+
+  const onCharSelected = (id) => {
+    setChar(id);
+  }
+
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <ErrorBoundery>
+          <RandomChar />
+        </ErrorBoundery>
+        <div className="char__content">
+          <ErrorBoundery>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundery>
+          <ErrorBoundery>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundery>
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  )
+}
+
+export default App;
