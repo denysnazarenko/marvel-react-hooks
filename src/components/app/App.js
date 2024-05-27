2e97dbd75b8c15e26bbd2168779d255d4486e0ed
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-05-27 11:53:43 +0300

    update MarvelService and added ComicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,30 @@
 import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
+// import RandomChar from "../randomChar/RandomChar";
+// import CharList from "../charList/CharList";
+// import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundery from "../errorBoundery/ErrorBoundery";
 
-import decoration from '../../resources/img/vision.png';
+import ComicsList from "../comicsList/ComicsList";
+
+// import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-  const [selectedChar, setChar] = useState(null);
+  // const [selectedChar, setChar] = useState(null);
 
-  const onCharSelected = (id) => {
-    setChar(id);
-  }
+  // const onCharSelected = (id) => {
+  //   setChar(id);
+  // }
 
   return (
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundery>
+        <errorBoundery>
+          <ComicsList />
+        </errorBoundery>
+        {/* <ErrorBoundery>
           <RandomChar />
         </ErrorBoundery>
         <div className="char__content">
@@ -30,7 +35,7 @@ const App = () => {
             <CharInfo charId={selectedChar} />
           </ErrorBoundery>
         </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
+        <img className="bg-decoration" src={decoration} alt="vision" /> */}
       </main>
     </div>
   )
